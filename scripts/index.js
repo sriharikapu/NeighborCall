8b4d172c64277c37b5a0cf85f4cdf7c048d8728a
Melise Kee <mjs.kee@gmail.com>
2020-03-30 19:50:28 +1300

    Add in success registration page

--- scripts/index.js
+++ scripts/index.js
@@ -13,61 +13,62 @@ window.onload = function () {
 	}
 
 	function renderSignUpPage() {
-		var html = `<form>
-		<div class="input-field col s6">
-			<i class="material-icons prefix">account_circle</i>
-			<input id="name" type="text" class="validate" required>
-			<label for="name">My name is</label>
-		</div>
+		var html = `
+		<form>
+			<div class="input-field col s6">
+				<i class="material-icons prefix">account_circle</i>
+				<input id="name" type="text" class="validate" required>
+				<label for="name">My name is</label>
+			</div>
 
-		<div id="divAddressInputs" class="row no-margin">
-			<!--<div class="input-field col s3">
-				<i class="material-icons prefix">home</i>
-				<input id="home" type="text" class="validate" required>
-				<label for="home">I live at </label>
-			</div>-->
+			<div id="divAddressInputs" class="row no-margin">
+				<!--<div class="input-field col s3">
+					<i class="material-icons prefix">home</i>
+					<input id="home" type="text" class="validate" required>
+					<label for="home">I live at </label>
+				</div>-->
 		
-			<div class="input-field col l4 s12">
-				<i class="material-icons prefix">home</i>
-				<input id="suburb" type="text" class="validate" required>
-				<label for="suburb">My suburb is </label>
+				<div class="input-field col l4 s12">
+					<i class="material-icons prefix">home</i>
+					<input id="suburb" type="text" class="validate" required>
+					<label for="suburb">My suburb is </label>
+				</div>
+
+				<div class="input-field col l4 s12">
+					<i class="material-icons prefix">home</i>
+					<input id="state" type="text" class="validate" required>
+					<label for="state">My state/region is </label>
+				</div>
+
+				<div class="input-field col l4 s12">
+					<i class="material-icons prefix">home</i>
+					<input id="country" type="text" class="validate" required>
+					<label for="country">My country is </label>
+				</div>
+
 			</div>
 
-			<div class="input-field col l4 s12">
-				<i class="material-icons prefix">home</i>
-				<input id="state" type="text" class="validate" required>
-				<label for="state">My state/region is </label>
+			<div class="input-field col s6">
+				<i class="material-icons prefix">phone</i>
+				<input id="telephone" type="tel" class="validate" required>
+				<label for="telephone">Contact me on</label>
 			</div>
 
-			<div class="input-field col l4 s12">
-				<i class="material-icons prefix">home</i>
-				<input id="country" type="text" class="validate" required>
-				<label for="country">My country is </label>
+			<div id="divTimeInput" class="input-field col s6">
+				<i class="material-icons prefix">schedule</i>
+				<input id="time" type="time" class="validate" required>
+				<label for="time">Contact me around</label>
 			</div>
 
-		</div>
-
-		<div class="input-field col s6">
-			<i class="material-icons prefix">phone</i>
-			<input id="telephone" type="tel" class="validate" required>
-			<label for="telephone">Contact me on</label>
-		</div>
-
-		<div id="divTimeInput" class="input-field col s6">
-			<i class="material-icons prefix">schedule</i>
-			<input id="time" type="time" class="validate" required>
-			<label for="time">Contact me around</label>
-		</div>
-
-		<div class="row">
-			<div class="input-field col s12">
-				<input id="btnSignUp" class="waves-effect waves-light btn-large" type="submit" value="Sign me up" />
+			<div class="row">
+				<div class="input-field col s12">
+					<input id="btnSignUp" class="waves-effect waves-light btn-large" type="submit" value="Sign me up" />
+				</div>
+				<div class="no-margin input-field col s12">
+					<a>or log in instead</a>
+				</div>
 			</div>
-			<div class="no-margin input-field col s12">
-				<a>or log in instead</a>
-			</div>
-		</div>
-	</form>`;
+		</form>`;
 
 		var div = document.getElementById("content");
 		div.innerHTML = html;
@@ -75,15 +76,13 @@ window.onload = function () {
 
 	function successfulRegistration() {
 		var html = `
-			<div class="row">
-				<div class="col 12 s12">
-					<i class="material-icons prefix">check_cirle</i>
-
-					<h2>Thank you!</h2>
-
-					<h4>You have successfully registered with NeighbourCall, please log in</h4>
-				</div>
+		<div class="row content-middle">
+			<div class="col s12">
+				<h2 class="success-title teal-text">Thank you!</h2>
+				<i class="material-icons large success-icon">check_circle</i>
+				<p>You have successfully registered with NeighbourCall, please <a class="teal-text">log in</a></p>
 			</div>
+		</div>
 		`;
 
 		var div = document.getElementById("content");
